9cd64dcd4e6e3653ba91cbb9a3636b04a777e9d3
nmhien2605 <nmhien2605@gmail.com>
2024-12-30 15:51:41 +0700

    change router to tab state

--- app/[locale]/(telegram)/(navbar)/page.tsx
+++ app/[locale]/(telegram)/(navbar)/page.tsx
@@ -1,7 +1,53 @@
+"use client";
+
+import { Suspense, useState } from "react";
+
+import Script from "next/script";
+
+import Loading from "@/components/common/Loading";
+import Navbar from "@/components/telegram/layout/Navbar";
+import MyBotPage from "@/components/telegram/views/bot/my-bot/MyBotPage";
+import History from "@/components/telegram/views/history/History";
 import HomePage from "@/components/telegram/views/home/HomePage";
+import LeaderboardPage from "@/components/telegram/views/leaderboard/LeaderboardPage";
+import { useSearchParams } from "@/i18n/routing";
 
 const Page = () => {
-  return <HomePage />;
+  const searchParams = useSearchParams();
+  const tabParam = +(searchParams.get("tab") || 0);
+
+  const [tab, setTab] = useState<number>(tabParam);
+
+  const renderTab = (): React.ReactNode => {
+    switch (tab) {
+      case 0:
+        return <HomePage />;
+      case 1:
+        return <History />;
+      case 2:
+        return <LeaderboardPage />;
+      case 3:
+        return <MyBotPage />;
+      default:
+        return <HomePage />;
+    }
+  };
+
+  return (
+    <Suspense fallback={<Loading />}>
+      <Script strategy="lazyOnload" />
+      <div className="relative">
+        {renderTab()}
+        <div className="fixed bottom-0 left-0 right-0 z-50">
+          <Navbar tab={tab} setTab={setTab} />
+        </div>
+      </div>
+    </Suspense>
+  );
 };
+
+// const Page = () => {
+//   return <HomePage />;
+// };
 
 export default Page;
--- components/telegram/layout/Navbar.tsx
+++ components/telegram/layout/Navbar.tsx
@@ -4,45 +4,63 @@ import { ReactNode } from "react";
 
 import Image from "next/image";
 
-import { Link, usePathname } from "@/i18n/routing";
+import { Link } from "@/i18n/routing";
 import { cn } from "@/libs/utils";
 
 const NavbarItem = ({
   icon,
   label,
   active = false,
-  href = "#",
+  href,
+  onClick,
 }: {
   icon: ReactNode;
   label: string;
   active?: boolean;
   href?: string;
+  onClick?: () => void;
 }) => {
-  return (
-    <Link href={href} className={cn("text-white", active && "text-primary")}>
-      <div className="relative flex w-16 flex-col items-center gap-2 whitespace-nowrap">
-        <div className="relative h-11 w-11">
-          {active && (
-            <div className="absolute bottom-0 left-1/2 h-20 w-28 -translate-x-1/2 rounded-full bg-black/80 blur-xl" />
+  const content = (
+    <div className="relative flex w-16 flex-col items-center gap-2 whitespace-nowrap">
+      <div className="relative h-11 w-11">
+        {active && (
+          <div className="absolute bottom-0 left-1/2 h-20 w-28 -translate-x-1/2 rounded-full bg-black/80 blur-xl" />
+        )}
+        <div
+          className={cn(
+            "absolute bottom-0 left-1/2 -translate-x-1/2 transition-all duration-300",
+            active && "bottom-4 scale-[1.75]",
           )}
-          <div
-            className={cn(
-              "absolute bottom-0 left-1/2 -translate-x-1/2 transition-all duration-300",
-              active && "bottom-4 scale-[1.75]",
-            )}
-          >
-            {icon}
-          </div>
+        >
+          {icon}
         </div>
-        <div className="text-xs">{label}</div>
       </div>
+      <div className="text-xs">{label}</div>
+    </div>
+  );
+
+  if (!href) {
+    return <button onClick={onClick}>{content}</button>;
+  }
+
+  return (
+    <Link
+      href={href}
+      onClick={onClick}
+      className={cn("text-white", active && "text-primary")}
+    >
+      {content}
     </Link>
   );
 };
 
-const Navbar = () => {
-  const pathname = usePathname();
-
+const Navbar = ({
+  tab = 0,
+  setTab,
+}: {
+  tab?: number;
+  setTab?: (_tab: number) => void;
+}) => {
   return (
     <div
       className="flex w-full justify-between rounded-t-[1.25rem] border-t border-primary/25 bg-gray-950/90 px-5 pb-6 pt-1 text-[#BEB7B1] shadow-[0_-20px_20px_0_rgba(0,0,0,0.8)]"
@@ -62,8 +80,10 @@ const Navbar = () => {
           </div>
         }
         label="Bot list"
-        active={pathname === "/"}
-        href="/"
+        // active={pathname === "/"}
+        href="?tab=0"
+        active={tab === 0}
+        onClick={() => setTab?.(0)}
       />
       <NavbarItem
         icon={
@@ -77,8 +97,11 @@ const Navbar = () => {
           </div>
         }
         label="History"
-        active={pathname.startsWith("/history")}
-        href="/history"
+        // active={pathname.startsWith("/history")}
+        // href="/history"
+        href="?tab=1"
+        active={tab === 1}
+        onClick={() => setTab?.(1)}
       />
       <NavbarItem
         icon={
@@ -92,8 +115,11 @@ const Navbar = () => {
           </div>
         }
         label="Leaderboard"
-        active={pathname.startsWith("/leaderboard")}
-        href="/leaderboard"
+        // active={pathname.startsWith("/leaderboard")}
+        // href="/leaderboard"
+        href="?tab=2"
+        active={tab === 2}
+        onClick={() => setTab?.(2)}
       />
       <NavbarItem
         icon={
@@ -107,8 +133,11 @@ const Navbar = () => {
           </div>
         }
         label="Create bot"
-        active={pathname.startsWith("/my-bot")}
-        href="/my-bot"
+        // active={pathname.startsWith("/my-bot")}
+        // href="/my-bot"
+        href="?tab=3"
+        active={tab === 3}
+        onClick={() => setTab?.(3)}
       />
     </div>
   );
